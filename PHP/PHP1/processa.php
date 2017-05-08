14b072406d48d10d637c46c117e973f1c21733a9
AglairNeto <neto.barroncassi@gmail.com>
2017-05-08 17:37:18 -0400

    PHP1
    Primeiro exercício de PHP

---
+++ PHP/PHP1/processa.php
@@ -0,0 +1,12 @@
+<?php
+	
+	$nome     = $_POST['nome'];
+	$email    = $_POST['email'];
+	$website  = $_POST['website'];
+	$mensagem = $_POST['mensagem'];
+
+	echo $nome . " <br> ";
+	echo $email . " <br> ";
+	echo $website . " <br> ";
+	echo $mensagem . " <br> ";  
+?>
