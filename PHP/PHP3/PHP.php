fb540184fa8d25e8dfa91541d46b0e27648db23c
AglairNeto <neto.barroncassi@gmail.com>
2017-06-19 17:14:59 -0400

    PHP3

---
+++ PHP/PHP3/PHP.php
@@ -0,0 +1,7 @@
+<?php
+    $nome = $_POST['nome'];
+    $email = $_POST['email'];
+    $website = $_POST['website'];
+    $mensagem = $_POST['mensagem'];
+    print("Nome: " . $nome . "<br/>Email: " . $email . "<br/>Website: " . $website . "<br/>Mensagem: " . $mensagem);
+?>
